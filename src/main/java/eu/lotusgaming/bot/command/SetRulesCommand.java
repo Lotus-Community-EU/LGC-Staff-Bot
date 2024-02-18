4d07650a72ad5679f61b15e6ed2824aa2be927d0
Christopher Wilhelm H. <info@kitsoft.at>
2024-02-18 22:44:25 +0100

    Removed dots, added red colored arrow.

--- src/main/java/eu/lotusgaming/bot/command/SetRulesCommand.java
+++ src/main/java/eu/lotusgaming/bot/command/SetRulesCommand.java
@@ -26,7 +26,7 @@ public class SetRulesCommand extends ListenerAdapter{
 					ruleChannel.sendMessage(transform(ruleBlock4())).queue();
 					SimpleDateFormat time = new SimpleDateFormat("dd/MM/yyyy - HH:mm");
 			        String stime = time.format(new Date());
-					ruleChannel.sendMessage("**We wish you all a lot of fun and a nice stay in the Lotus Gaming Discord**\n*The Lotus Staff Team*\n \nRules has been updated last at: ***" + stime + "***").queue();
+					ruleChannel.sendMessage("\n**We wish you all a lot of fun and a nice stay in the Lotus Gaming Discord**\n*The Lotus Staff Team*\n \nRules has been updated last at: ***" + stime + "***").queue();
 				}else {
 					event.deferReply(true).addContent("The Channel must be a text channel!").queue();
 				}
@@ -37,11 +37,11 @@ public class SetRulesCommand extends ListenerAdapter{
 	List<String> ruleBlock1() {
 		List<String> rules = new ArrayList<>();
 		rules.add("# Lotus Gaming Community Discord Rules");
-		//rules.add("Read the rules in different languages: https://lotuscommunity.eu/");
+		rules.add("Read the rules in different languages: https://lotuscommunity.eu/");
 		rules.add("\n");
 		rules.add("## Section 1 - Following rulesets and policies apply");
-		rules.add("§1.1.) Discord [Terms of Service](<https://discord.com/terms>) & [Community Guidelines](<https://discord.com/guidelines>)");
-		//rules.add("§1.2.) Lotus Gaming Community Rules");
+		rules.add("<:redarrow:1208890097039314955> §1.1.) Discord [Terms of Service](<https://discord.com/terms>) & [Community Guidelines](<https://discord.com/guidelines>)");
+		rules.add("<:redarrow:1208890097039314955> §1.2.) Lotus Gaming Community Global Rules");
 		rules.add("\n");
 		return rules;
 	}
@@ -49,19 +49,19 @@ public class SetRulesCommand extends ListenerAdapter{
 	List<String> ruleBlock2() {
 		List<String> rules = new ArrayList<>();
 		rules.add("## Section 2 - Content & Usage");
-		rules.add("<:red_dot:1207673800666255390> §2.1.) English is the only language permitted unless stated otherwise.");
-		rules.add("<:red_dot:1207673800666255390> §2.2.) Be respectful and polite to everyone - treat others how you would want to be treated.");
-		rules.add("<:red_dot:1207673800666255390> §2.3.) Channels, threads & forums should only be used for their intended purpose.");
-		rules.add("<:red_dot:1207673800666255390> §2.4.) Channel descriptions consitute as part of the discord rules.");
+		rules.add("<:redarrow:1208890097039314955> §2.1.) English is the only language permitted unless stated otherwise.");
+		rules.add("<:redarrow:1208890097039314955> §2.2.) Be respectful and polite to everyone - treat others how you would want to be treated.");
+		rules.add("<:redarrow:1208890097039314955> §2.3.) Channels, threads & forums should only be used for their intended purpose.");
+		rules.add("<:redarrow:1208890097039314955> §2.4.) Channel descriptions consitute as part of the discord rules.");
 		rules.add("### Prohibited:");
-		rules.add("<:red_dot:1207673800666255390> §2.5.) Swearing, insulting, offending, provoking or aggravating others.");
-		rules.add("<:red_dot:1207673800666255390> §2.6.) Asking or begging others to buy or gift products for you.");
-		rules.add("<:red_dot:1207673800666255390> §2.7.) NSFW, epilepsy-inducing, sexually suggestive, racist, homophobic, sexist or other discriminatory & inappropiate content.");
-		rules.add("<:red_dot:1207673800666255390> §2.8.) Starting or contributing to political or other sensitive discussions which may lead to arguments.");
-		rules.add("<:red_dot:1207673800666255390> §2.9.) Spamming, such as but not limited to: messages, emotes, symbols, images, reactions, rejoining, (ghost) mentioning, tickets, ASCII art, etc..");
-		rules.add("<:red_dot:1207673800666255390> §2.10.) Discussing punishments, reports, appeals, applications & complaints about team members");
-		rules.add("<:red_dot:1207673800666255390> §2.11.) Requesting team members to moderate in-game. Use the in-game or web report system instead.");
-		rules.add("<:red_dot:1207673800666255390> §2.12.) Any mention (e.g. emojis, channels, forums, user) within the Minecraft Chat Bridge.");
+		rules.add("<:redarrow:1208890097039314955> §2.5.) Swearing, insulting, offending, provoking or aggravating others.");
+		rules.add("<:redarrow:1208890097039314955> §2.6.) Asking or begging others to buy or gift products for you.");
+		rules.add("<:redarrow:1208890097039314955> §2.7.) NSFW, epilepsy-inducing, sexually suggestive, racist, homophobic, sexist or other discriminatory & inappropiate content.");
+		rules.add("<:redarrow:1208890097039314955> §2.8.) Starting or contributing to political or other sensitive discussions which may lead to arguments.");
+		rules.add("<:redarrow:1208890097039314955> §2.9.) Spamming, such as but not limited to: messages, emotes, symbols, images, reactions, rejoining, (ghost) mentioning, tickets, ASCII art, etc..");
+		rules.add("<:redarrow:1208890097039314955> §2.10.) Discussing punishments, reports, appeals, applications & complaints about team members");
+		rules.add("<:redarrow:1208890097039314955> §2.11.) Requesting team members to moderate in-game. Use the in-game or web report system instead.");
+		rules.add("<:redarrow:1208890097039314955> §2.12.) Any mention (e.g. emojis, channels, forums, user) within the Minecraft Chat Bridge.");
 		rules.add("\n");
 		return rules;
 	}
@@ -69,11 +69,11 @@ public class SetRulesCommand extends ListenerAdapter{
 	List<String> ruleBlock3() {
 		List<String> rules = new ArrayList<>();
 		rules.add("## Section 3 - Advertisments");
-		rules.add("<:green_dot:1207673798753648711> §3.1.) Advertising isn't allowed anywhere, including nicknames and unsolicited direct messages.");
+		rules.add("<:redarrow:1208890097039314955> §3.1.) Advertising isn't allowed anywhere, including nicknames and unsolicited direct messages.");
 		rules.add(" ");
 		rules.add("## Section 4 - Profile");
-		rules.add("<:rose_dot:1207673802327199744> §4.1.) Your profile must be appropiate, including nickname, avatar, banner, status, pronouns & about me.");
-		rules.add("<:rose_dot:1207673802327199744> §4.2.) You nickname must only consist of alphanumeric characters. Emojis are allowed when combined with alphanumeric characters.");
+		rules.add("<:redarrow:1208890097039314955> §4.1.) Your profile must be appropiate, including nickname, avatar, banner, status, pronouns & about me.");
+		rules.add("<:redarrow:1208890097039314955> §4.2.) You nickname must only consist of alphanumeric characters. Emojis are allowed when combined with alphanumeric characters.");
 		rules.add("\n");
 		return rules;
 	}
@@ -81,18 +81,18 @@ public class SetRulesCommand extends ListenerAdapter{
 	List<String> ruleBlock4() {
 		List<String> rules = new ArrayList<>();
 		rules.add("## Section 5 - Punishments");
-		rules.add("<:red_dot:1207673800666255390> §5.1.) Punishments are structured as followed:");
-		rules.add("- 2x verbal warnings");
-		rules.add("- 2x logged warnings");
-		rules.add("- 1 week timeout");
-		rules.add("- 1 week ban");
-		rules.add("- 4 week ban");
-		rules.add("- consecutive bans are 90 days");
-		rules.add("<:red_dot:1207673800666255390> §5.2.) Punishments that expired 12 month ago are ignored.");
-		rules.add("<:red_dot:1207673800666255390> §5.3.) Punishments carry across discord accounts.");
-		rules.add("<:red_dot:1207673800666255390> §5.4.) We reserve the right to revoke your access to our ticket system");
-		rules.add("<:red_dot:1207673800666255390> §5.5.) Management reserve the right to override the punishment ladder.");
-		rules.add("<:red_dot:1207673800666255390> §5.6.) You can contest any punishments you have received via the feedback system, selecting the Discord Punishments category when creating a ticket.");
+		rules.add("<:redarrow:1208890097039314955> §5.1.) Punishments are structured as followed:");
+		rules.add("  - 2x verbal warnings");
+		rules.add("  - 2x logged warnings");
+		rules.add("  - 1 week timeout");
+		rules.add("  - 1 week ban");
+		rules.add("  - 4 week ban");
+		rules.add("  - consecutive bans are 90 days");
+		rules.add("<:redarrow:1208890097039314955> §5.2.) Punishments that expired 12 month ago are ignored.");
+		rules.add("<:redarrow:1208890097039314955> §5.3.) Punishments carry across discord accounts.");
+		rules.add("<:redarrow:1208890097039314955> §5.4.) We reserve the right to revoke your access to our ticket system");
+		rules.add("<:redarrow:1208890097039314955> §5.5.) Management reserve the right to override the punishment ladder.");
+		rules.add("<:redarrow:1208890097039314955> §5.6.) You can contest any punishments you have received via the feedback system, selecting the Discord Punishments category when creating a ticket.");
 		rules.add("\n");
 		return rules;
 	}
