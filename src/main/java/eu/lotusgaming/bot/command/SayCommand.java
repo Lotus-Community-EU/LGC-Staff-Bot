cfebad516fbce1ecef35cdbddc051e882c004bad
Maurice H. <development@lotuscommunity.eu>
2024-09-21 11:56:37 +0200

    Added Modlog Component to see who used the command with content.

--- src/main/java/eu/lotusgaming/bot/command/SayCommand.java
+++ src/main/java/eu/lotusgaming/bot/command/SayCommand.java
@@ -1,6 +1,8 @@
 //Created by Christopher at 13.07.2024
 package eu.lotusgaming.bot.command;
 
+import eu.lotusgaming.bot.handlers.modlog.ModlogController;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -16,6 +18,11 @@ public class SayCommand extends ListenerAdapter {
 				TextChannel channel = event.getOption("targetchannel").getAsChannel().asTextChannel();
 				channel.sendMessage(text).queue();
 				event.deferReply(true).addContent("The message has been sent into " + channel.getAsMention()).queue();
+				EmbedBuilder eb = ModlogController.baseEmbed(event.getGuild());
+				eb.setTitle(event.getUser().getName() + " used /say command");
+				eb.setDescription("Message Content: " + text);
+				eb.setColor(ModlogController.green);
+				ModlogController.sendMessage(eb, event.getGuild());
 			}else {
 				event.deferReply(true).addContent("The channel you have mentioned is not a text channel!").queue();
 			}
