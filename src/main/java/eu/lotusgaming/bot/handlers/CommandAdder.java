1a4ec06b568a958a429ab105e135cd7ca0d3262a
Maurice H. <development@lotuscommunity.eu>
2025-04-20 13:09:03 +0200

    Reflected the latest JDA changes to avoid use of deprecated Methods.

--- src/main/java/eu/lotusgaming/bot/handlers/CommandAdder.java
+++ src/main/java/eu/lotusgaming/bot/handlers/CommandAdder.java
@@ -3,6 +3,7 @@ package eu.lotusgaming.bot.handlers;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.Command.Type;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -19,17 +20,17 @@ public class CommandAdder {
 					//Commands regarding the Ticket System.
 					Commands.slash("setticketchannel", "Sets the Ticket Channel")
 					.addOption(OptionType.CHANNEL, "channel", "The Channel where the message should be sent to.")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
 					Commands.slash("tickets", "See all tickets an user has ever made.")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.USER, "user", "The user to lookup")
 					.addOption(OptionType.INTEGER, "userid", "The user id to lookup")
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
 					Commands.slash("tickethistory", "See the chat of that ticket")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.INTEGER, "ticketid", "The ticket id to lookup", true)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
@@ -37,7 +38,7 @@ public class CommandAdder {
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.BAN_MEMBERS, Permission.KICK_MEMBERS)),
 					
 					Commands.slash("ticket", "Main command for the Ticket System")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.BAN_MEMBERS, Permission.KICK_MEMBERS))
 					.addSubcommands(
                             new SubcommandData("adduser", "Adds an user to this ticket")
@@ -59,7 +60,7 @@ public class CommandAdder {
 					Commands.context(Type.MESSAGE, "Report this!"),
 					
 					Commands.slash("ticketban", "Bans a user/id from using the ticket system")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.INTEGER, "userid", "The userid to ban")
 					.addOption(OptionType.USER, "user", "The user to ban")
 					.addOption(OptionType.STRING, "reason", "The Reason for the ban")
@@ -68,33 +69,33 @@ public class CommandAdder {
 					
 					//Commands regarding the Suggestion System.
 					Commands.slash("setsuggestionboardchannel", "Sets the Suggestion Info Channel")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.CHANNEL, "channel", "The channel where the message should be sent to")
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
 					Commands.slash("setsuggestionmessagechannel", "Sets the Suggestionboard Channel")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.CHANNEL, "channel", "The channel where the message should be sent to")
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
 					Commands.slash("setrules", "Sends the rules")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.CHANNEL, "channel", "The channel where the rules should be sent to.", true)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
 					Commands.slash("setinfo", "Sends the info")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.CHANNEL, "channel", "The channel where the rules should be sent to.", true)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)),
 					
 					Commands.slash("kick", "Kicks a member")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.USER, "user", "The user to kick", true)
 					.addOption(OptionType.STRING, "reason", "The reason why the user gets kicked", true)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR, Permission.KICK_MEMBERS)),
 					
 					Commands.slash("ban", "Bans a member")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.USER, "user", "The user to ban", true)
 					.addOption(OptionType.STRING, "reason", "The reason why the user gets banned", true)
 					.addOption(OptionType.INTEGER, "time", "Time for the ban's persistiency (0 for Permanent)", true)
@@ -102,13 +103,13 @@ public class CommandAdder {
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR, Permission.BAN_MEMBERS)),
 					
 					Commands.slash("warn", "Warns a member")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.USER, "user", "The user to warn", true)
 					.addOption(OptionType.STRING, "reason", "The reason why the user gets warned", true)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR, Permission.BAN_MEMBERS, Permission.KICK_MEMBERS)),
 					
 					Commands.slash("mute", "Mutes a member (Utilising timeout)")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.USER, "user", "The user to mute", true)
 					.addOption(OptionType.STRING, "reason", "The reason why the user got muted", true)
 					.addOption(OptionType.INTEGER, "time", "Time for the ban's persistiency (0 for Permanent)", true)
@@ -116,22 +117,22 @@ public class CommandAdder {
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR, Permission.BAN_MEMBERS, Permission.KICK_MEMBERS)),
 					
 					Commands.slash("say", "Let the bot talk")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.CHANNEL, "targetchannel", "The Channel the bot should write in", true)
 					.addOption(OptionType.STRING, "text", "The text the bot should write", true)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR, Permission.MANAGE_CHANNEL, Permission.MANAGE_SERVER)),
 					
 					Commands.slash("purge", "Purge messages from in this channel.")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.INTEGER, "messages", "The count of messages to be deleted (max 100)", true)
 					.addOption(OptionType.USER, "member", "Only delete the messages from this user (optional)")
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL, Permission.MESSAGE_MANAGE, Permission.MANAGE_SERVER)),
 					
 					Commands.slash("set-status", "Modify the online status for the bot")
-					.setGuildOnly(false),
+					.setContexts(InteractionContextType.ALL),
 					
 					Commands.slash("set-activity", "Modify the activity status for the bot")
-					.setGuildOnly(false)
+					.setContexts(InteractionContextType.ALL)
 					.addOption(OptionType.STRING, "option", "Choose what kind of activity the bot should display", true, true)
 					.addOption(OptionType.STRING, "text", "The text followed up", true),
 					
@@ -142,15 +143,15 @@ public class CommandAdder {
 					.addOption(OptionType.USER, "member", "The member to lookup."),
 					
 					Commands.slash("mclookup", "Player Lookup on Lotus for Minecraft")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOption(OptionType.STRING, "player", "The player to lookup", true, true),
 					
 					Commands.slash("serverinfo", "Looking up specific game server")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addOptions(new OptionData(OptionType.STRING, "server", "Specify the Game Server").setAutoComplete(true)),
 					
 					Commands.slash("birthday", "Main Command for the birthday function")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.addSubcommands(
 							new SubcommandData("set", "Sets the birthday")
 							.addOption(OptionType.STRING, "date", "The Date to set (dd/MM -> 26/07)", true),
@@ -161,7 +162,7 @@ public class CommandAdder {
 							)
 					
 					/*Commands.slash("customcommands", "Main Command for custom commands")
-					.setGuildOnly(true)
+					.setContexts(InteractionContextType.GUILD)
 					.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
 					.addSubcommands(
 							new SubcommandData("add", "Adds an custom command")
