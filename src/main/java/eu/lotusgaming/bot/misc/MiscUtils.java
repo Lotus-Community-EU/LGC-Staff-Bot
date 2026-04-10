0855c282d76203d0c7449020fcdd8ecc6795ac0e
Maurice H. <maurice.hoefner@gmail.com>
2026-04-10 20:48:40 +0200

    Add color constants to MiscUtils for improved UI feedback

--- src/main/java/eu/lotusgaming/bot/misc/MiscUtils.java
+++ src/main/java/eu/lotusgaming/bot/misc/MiscUtils.java
@@ -1,6 +1,7 @@
 //Created by Chris Wille at 11.02.2024
 package eu.lotusgaming.bot.misc;
 
+import java.awt.Color;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,6 +9,11 @@ public class MiscUtils {
 	
 	public static long public_guild = 1153419306789507125l;
 	public static long staff_guild = 1066812641768640542l;
+
+	public static Color ok = Color.decode("#1eff00");
+	public static Color err = Color.decode("#ff0000");
+	public static Color warn_easy = Color.decode("#ffff00");
+	public static Color warn_severe = Color.decode("#ff8000");
 	
 	public static String retDate(OffsetDateTime odt, String pattern) {
 		return odt.format(DateTimeFormatter.ofPattern(pattern));
